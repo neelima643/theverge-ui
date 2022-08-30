9ccad35af958391d7bf4ae59791191698594ab45
neelima <neelimasajit@gmail.com>
2022-08-30 14:56:50 +0530

    Feat: Add responsive header and navlist

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,26 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'header-image': "url('../src/images/bw.png')",
+        'header-image': "url('../src/images/bgimg.jpg')",
       },
       fontFamily: {
         'heading-font': ["Arial Narrow", 'Arial', 'sans-serif'],
+      },
+
+      screens: {
+        'mobile' : '400px',
+      },
+
+      colors: {
+        'nav-list' : '#404040',
+      },
+
+      margin: {
+        '20p': '22%',
+      },
+
+      borderWidth: {
+        'half': '0.3px'
       }
     },
   },
