f364cd9b94f545a88faff67f0633f786a218498f
neelima <neelimasajit@gmail.com>
2022-09-05 14:20:35 +0530

    Feat: Add scroll to menuitems

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-const { brotliDecompressSync } = require("zlib");
-
-function navdisplay() {
-  body.style.overflow = "hidden";
-}
-
-function navclose() {
-  for (i = 0; i < 3; i++) {
-    const navdivid = document.getElementsByClassName("navdiv")[i];
-    if (navdivid.style.display === "none") {
-      navdivid.style.display = "block";
-    } else {
-      navdivid.style.display = "none";
-    }
+const navclose1 = () => {
+  const navid1 = document.getElementById("navid1");
+  if (navid1.style.display === "none") {
+    navid1.style.display = "block";
+    navid2.style.display = "none";
+    navid3.style.display = "none";
+    body.style.overflow = "hidden";
+    navid1.style.display = "block";
+    navid1.style.overflowY = "scroll";
+  } else {
+    navid1.style.display = "none";
+    body.style.overflowY = "scroll";
   }
-}
+};
+
+const navclose2 = () => {
+  const navid2 = document.getElementById("navid2");
+  if (navid2.style.display === "none") {
+    navid2.style.display = "block";
+    navid1.style.display = "none";
+    navid3.style.display = "none";
+    body.style.overflow = "hidden";
+  } else {
+    navid2.style.display = "none";
+    body.style.overflowY = "scroll";
+  }
+};
+
+const navclose3 = () => {
+  const navid3 = document.getElementById("navid3");
+  if (navid3.style.display === "none") {
+    navid3.style.display = "block";
+    navid2.style.display = "none";
+    navid1.style.display = "none";
+    body.style.overflow = "hidden";
+  } else {
+    navid3.style.display = "none";
+    body.style.overflowY = "scroll";
+  }
+};
